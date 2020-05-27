a0aa9659efbdcc9b021da82b86a5aca322dec4d0
nikunj0777 <nikunjpatel07777@gmail.com>
2020-05-27 16:00:11 +1200

    4 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 //it provides metadeata about component
 @Component({
   selector: 'app-product-alerts',
@@ -8,9 +9,14 @@ import { Input } from '@angular/core';
 })
 //class, which handels functionality of the components.
 export class ProductAlertsComponent implements OnInit {
+
+  
   
   //passing the property value
 @Input() product;
+
+//Imported Output and EventEmitter on the top of the file.
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
