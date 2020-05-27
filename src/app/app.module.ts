a8eb0bfdb738e1e42627fd593eb906ddf11bdf6a
nikunj0777 <nikunjpatel07777@gmail.com>
2020-05-27 16:33:09 +1200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,28 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
+//path setup like, by the product id  we can get product details.
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
 
 
